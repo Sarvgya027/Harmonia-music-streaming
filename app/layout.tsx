cf7c1ce96771ba9fc653cc6bd7c682cd01db2da6
sarvgyaverma027@gmail.com <sarvgyaverma027@gmail.com>
2024-11-16 18:10:58 +0530

    supabase client auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Sidebar } from "@/components/Sidebar";
+import SupabaseProvier from "@/providers/SupabaseProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvier>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvier>
       </body>
     </html>
   );
